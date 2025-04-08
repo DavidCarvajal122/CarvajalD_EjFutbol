a137d7e344dbb2f72a6b464f1519ffd75fa185df
DavidCarvajal122 <david.carvajal.delrosario@udla.edu.ec>
2025-04-07 20:38:46 -0500

    Configuración de EquipoController

---
+++ CarvajalD_EjFutbol/Controllers/EquipoController.cs
@@ -0,0 +1,20 @@
+﻿using CarvajalD_EjFutbol.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarvajalD_EjFutbol.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public ActionResult View()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+            return View(equipos);  
+        }
+    }
+}
--- CarvajalD_EjFutbol/Models/Equipo.cs
+++ CarvajalD_EjFutbol/Models/Equipo.cs
@@ -1,13 +1,25 @@
-﻿namespace CarvajalD_EjFutbol.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarvajalD_EjFutbol.Models
 {
     public class Equipo
     {
+        [Key]
         public int Id { get; set; } // Estructura de atributo 
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Nombre del Equipo")]
         public string Nombre { get; set; }
+        [Range(0, 100)]
         public int PartidosJugados { get; set; }
+        [Range(0, 100)]
         public int PartidosGanados { get; set; }
+        [Range(0, 100)]
         public int PartidosPerdidos { get; set ;}
+        [Range(0, 100)]
         public int PartidosEmpatados { get; set; }
+     
 
     }
 }
