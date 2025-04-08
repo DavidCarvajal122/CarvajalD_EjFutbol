11875e43cc2eb2e4601f77cb1d116215b234ab82
DavidCarvajal122 <david.carvajal.delrosario@udla.edu.ec>
2025-04-07 19:31:26 -0500

    Modelo Equipo

---
+++ CarvajalD_EjFutbol/Models/Equipo.cs
@@ -0,0 +1,13 @@
+﻿namespace CarvajalD_EjFutbol.Models
+{
+    public class Equipo
+    {
+        public int Id { get; set; } // Estructura de atributo 
+        public string Nombre { get; set; }
+        public int PartidosJugados { get; set; }
+        public int PartidosGanados { get; set; }
+        public int PartidosPerdidos { get; set ;}
+        public int PartidosEmpatados { get; set; }
+
+    }
+}
